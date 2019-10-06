6c13a66766151d57f6f5499910a4fa25645bee22
Rick Rothenberg <rrothenb@yahoo.com>
2019-10-06 05:41:19 -0600

    Allow optionally specifying which tests to run

--- src/cli/vdr-test.js
+++ src/cli/vdr-test.js
@@ -9,6 +9,7 @@ commander
     .version(pkg.version)
     .description(pkg.description)
     .arguments('<account>')
+    .arguments('[<GLOB|DIR|FILE[:LINE]>]')
     .option('-s, --skip-setup', 'run the tests against the current setup')
     .parse(process.argv);
 
@@ -20,10 +21,12 @@ if (commander.args.length == 0) {
 const account = commander.args[0]
 const testDir = process.cwd()
 
+const tests = commander.args.length == 2 ? commander.args[1] : 'tests'
+
 process.env.account = account
 process.env.testDir = testDir
 if (commander.skipSetup) {
     process.env.skipSetup = 'true'
 }
 process.chdir(`${__dirname}/../..`)
-spawn('node_modules/cucumber/bin/cucumber-js', [ '--require', 'src/cucumber/', `${testDir}/tests/`], {stdio: "inherit"})
+spawn('node_modules/cucumber/bin/cucumber-js', [ '--require', 'src/cucumber/', `${testDir}/${tests}`], {stdio: "inherit"})
